4f3d3c925138f9f6e888b8ad69561345e11d1f86
rupalijain1518 <39921763+rupalijain1518@users.noreply.github.com>
2020-09-04 16:56:51 +0530

    build added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/layout/header';
 import Retailers from './components/dashboard/retailer/listRetailers';
 import Packages from './components/dashboard/packages/packages'
 import addPackage from './components/dashboard/packages/addPackage'
-import { BrowserRouter as Router , Route , Switch , Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import AssignPackage from './components/dashboard/packages/assignPackage'
 import Login from './components/auth/login';
 import NotFound from './components/misc/notFound';
@@ -18,26 +18,26 @@ import logout from './components/misc/logout'
 import firebase from './config/fire'
 import search from './components/dashboard/search'
 import LoggedInLinks from './components/layout/loggedInLinks';
-function AuthenticatedRoute({component: Component, authenticated, ...rest}) {
+function AuthenticatedRoute({ component: Component, authenticated, ...rest }) {
   return (
     <Route
       {...rest}
       render=
-      {(props) => authenticated === false || null ?        <Redirect to={{pathname: '/login', state: {from: props.location}}} />
-    /*  :   authenticated === null?  <Component {...props} {...rest} /> */: <Component {...props} {...rest} />  
+      {(props) => authenticated === false || null ? <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+    /*  :   authenticated === null?  <Component {...props} {...rest} /> */ : <Component {...props} {...rest} />
       }
-   />
+    />
   )
 }
 
 
 class App extends Component {
-  
+
   constructor() {
     super();
     this.setCurrentUser = this.setCurrentUser.bind(this);
-   this.state = {
-      authenticated: null,
+    this.state = {
+      authenticated: false,
       currentUser: null,
     };
   }
@@ -45,28 +45,28 @@ class App extends Component {
   setCurrentUser(u) {
     if (u) {
       this.setState({
-        currentUser: u,
+        currentUser: true,
         authenticated: true
       })
     } else {
       this.setState({
-        currentUser: null,
-        authenticated: null
+        currentUser: false,
+        authenticated: false
       })
     }
   }
-  
+
   componentDidMount() {
     this.removeAuthListener = firebase.auth().onAuthStateChanged((u) => {
       if (u) {
         this.setState({
           authenticated: true,
-          currentUser: u,
+          currentUser: true,
         })
-  } else {
+      } else {
         this.setState({
-          authenticated: null,
-          currentUser: null,
+          authenticated: false,
+          currentUser: false,
         })
       }
     })
@@ -77,52 +77,52 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-  <Router>
+        <Router>
 
-     <Header authenticated = {this.state.authenticated}/>
+          <Header authenticated={this.state.authenticated} />
 
-   <Switch>
-    <Route exact path='/'   render={(props) => { return <Login setCurrentUser={this.setCurrentUser} {...props} />}}/>
-    <Route exact path = "/login" render={(props) => { return <Login setCurrentUser={this.setCurrentUser} {...props} />}}/>
-     
-    <AuthenticatedRoute authenticated={this.state.currentUser} exact path="/users" component={listUsers}/>
-     <AuthenticatedRoute authenticated={this.state.currentUser} exact path = "/retailers" component = {Retailers} /> 
-     <AuthenticatedRoute authenticated={this.state.currentUser} exact path = "/packages" component = {Packages} />
-     
-     <AuthenticatedRoute authenticated={this.state.currentUser} exact path = "/addPackages" component = {addPackage} />
-     <AuthenticatedRoute authenticated={this.state.currentUser} 
-     exact path = "/search" component = {search} />
-     
-      <AuthenticatedRoute authenticated={this.state.currentUser} exact path='/showRetailer/:id' component={RetailerDetail} />
-      
-      <AuthenticatedRoute 
-      authenticated={this.state.currentUser} 
-      exact path='/assignPackage/:id' 
-      component={AssignPackage} />
-      
-      <AuthenticatedRoute 
-      authenticated={this.state.currentUser}
-       exact path='/showUser/:id' 
-       component={UserDetail} />
-      
-      <AuthenticatedRoute
-       authenticated={this.state.currentUser}
-       exact path = "/showPackage/:id" 
-       component = {PackageDetail}/>
+          <Switch>
+            <Route exact path='/' render={(props) => { return <Login setCurrentUser={this.setCurrentUser} {...props} /> }} />
+            <Route exact path="/login" render={(props) => { return <Login setCurrentUser={this.setCurrentUser} {...props} /> }} />
 
-     
-      <AuthenticatedRoute 
-      authenticated={this.state.currentUser} 
-      exact path = "/addPackages" 
-      component = {addPackage} />
-      <Route  exact path = "/logout" component ={logout}/>
-     
-      
-      <Route  component = {NotFound} /> 
-</Switch>
-    
-  </Router>
-  </div>
+            <AuthenticatedRoute authenticated={this.state.currentUser} exact path="/users" component={listUsers} />
+            <AuthenticatedRoute authenticated={this.state.currentUser} exact path="/retailers" component={Retailers} />
+            <AuthenticatedRoute authenticated={this.state.currentUser} exact path="/packages" component={Packages} />
+
+            <AuthenticatedRoute authenticated={this.state.currentUser} exact path="/addPackages" component={addPackage} />
+            <AuthenticatedRoute authenticated={this.state.currentUser}
+              exact path="/search" component={search} />
+
+            <AuthenticatedRoute authenticated={this.state.currentUser} exact path='/showRetailer/:id' component={RetailerDetail} />
+
+            <AuthenticatedRoute
+              authenticated={this.state.currentUser}
+              exact path='/assignPackage/:id'
+              component={AssignPackage} />
+
+            <AuthenticatedRoute
+              authenticated={this.state.currentUser}
+              exact path='/showUser/:id'
+              component={UserDetail} />
+
+            <AuthenticatedRoute
+              authenticated={this.state.currentUser}
+              exact path="/showPackage/:id"
+              component={PackageDetail} />
+
+
+            <AuthenticatedRoute
+              authenticated={this.state.currentUser}
+              exact path="/addPackages"
+              component={addPackage} />
+            <Route exact path="/logout" component={logout} />
+
+
+            <Route component={NotFound} />
+          </Switch>
+
+        </Router>
+      </div>
     );
   }
 }
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,20 +1,20 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import LoggedInLinks from './loggedInLinks'
 import LogoutLinks from './logoutLinks'
-class Header extends Component{
+class Header extends Component {
   constructor(props) {
     super(props)
   }
 
-render(){
-  return(    
-    
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" >InfoDeck</a>
-      {this.props.authenticated  === null || false     ?  <LogoutLinks/> :<LoggedInLinks/> }
-    </nav>
+  render() {
+    return (
+
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" >InfoDeck</a>
+        {this.props.authenticated === false || null ? <LogoutLinks /> : <LoggedInLinks />}
+      </nav>
     )
-    
-}
+
+  }
 }
 export default Header;
