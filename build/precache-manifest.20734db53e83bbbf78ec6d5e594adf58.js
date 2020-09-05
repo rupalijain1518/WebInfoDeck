c1103bfca2b489525324b1238d879fdee5014108
rupalijain1518 <39921763+rupalijain1518@users.noreply.github.com>
2020-09-05 16:07:16 +0530

    build added

---
+++ build/precache-manifest.20734db53e83bbbf78ec6d5e594adf58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db6feb0ee64a35a17ba2a40277e25e1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d108349d5236f45a1972",
+    "url": "./static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7c9b22d72c76f2f5645e",
+    "url": "./static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d108349d5236f45a1972",
+    "url": "./static/js/2.698b138e.chunk.js"
+  },
+  {
+    "revision": "f65791bd18bd3159d0053654b9d757a4",
+    "url": "./static/js/2.698b138e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9b22d72c76f2f5645e",
+    "url": "./static/js/main.83fe440b.chunk.js"
+  },
+  {
+    "revision": "0b7d30b3d4021872e41b",
+    "url": "./static/js/runtime-main.be319a95.js"
+  }
+]);
